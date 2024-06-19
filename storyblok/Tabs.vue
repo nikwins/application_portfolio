22982e44ef7bd3b8cf288de6ac68d2a548a7406e
Niklas Wingender <business@niklas-wingender.de>
2024-06-19 15:50:55 +0200

    [FIX] Tabs Hover State

--- storyblok/Tabs.vue
+++ storyblok/Tabs.vue
@@ -24,7 +24,7 @@ const props = defineProps<{
                         'w-full py-base px-lg mb-mdsm bg-gray-800 border-b-2 text-white transition-colors rounded',
                         'ui-selected:border-primary ui-selected:font-bold ui-selected:text-primary ui-selected:rounded-b-none',
                         'hover:!border-primary hover:!rounded-b-none focus-visible:outline-none sm:[&(not:(:last-child))]:mr-base sm:mb-0 sm:w-auto lg:[&(not:(:last-child))]:mr-lg',
-                        props.blok.has_experience_tabs ? 'lg:rounded-b-none ui-not-selected:!border-gray-800' : 'ui-not-selected:border-bodyBgColor'
+                        props.blok.has_experience_tabs ? 'lg:rounded-b-none ui-not-selected:!border-gray-800 hover:ui-not-selected:!border-primary' : 'ui-not-selected:border-bodyBgColor'
                     ]"
                 >{{ tab.title }}</Tab>
             </TabList>
