3b6a9d7ac881eaf9d6d16e221af5b275e70ec77b
Niklas Wingender <business@niklas-wingender.de>
2024-06-19 13:54:22 +0200

    [TASK] Add SkillMeter and Skills components

---
+++ storyblok/SkillMeter.vue
@@ -0,0 +1,98 @@
+<script setup lang="ts">
+import { vIntersectionObserver } from '@vueuse/components'
+
+const props = defineProps<{
+    blok: SkillMeter,
+    delay: number
+}>()
+
+interface SkillMeter {
+    skill: string,
+    experience: number,
+    logo: {
+        filename: string,
+        title: string,
+        alt: string,
+    }
+}
+
+const isInViewport = ref(false)
+const wasInViewport = ref(false)
+const isScrollingUp = ref(false)
+const lastScrollTop = ref(0)
+
+const onIntersectionObserver = ([{ isIntersecting }]: IntersectionObserverEntry[]) => {
+    if (isIntersecting) {
+        isInViewport.value = true
+        if (!wasInViewport.value) {
+            wasInViewport.value = true
+        }
+    } else {
+        isInViewport.value = false
+    }
+}
+
+const skillLevel = computed(() => {
+    if ( (isInViewport.value) || (wasInViewport.value && isScrollingUp.value) ) {
+        return props.blok.experience
+    }
+    return 0
+});
+
+const animationClasses = computed(() => {
+    if (isInViewport.value) {
+        return 'opacity-100 transition-all duration-1000'
+    }
+    if (wasInViewport.value && isScrollingUp.value) {
+        return 'opacity-100'
+    }
+    return 'opacity-0'
+});
+
+const transitionDelay = computed(() => {
+    if (props.delay && isInViewport.value) {
+        return `transition-delay: ${props.delay}ms`
+    }
+    return ''
+})
+
+window.addEventListener('scroll', () => {
+    const currentScrollTop = window.scrollY || document.documentElement.scrollTop
+    isScrollingUp.value = currentScrollTop < lastScrollTop.value
+    lastScrollTop.value = currentScrollTop
+})
+</script>
+<template>
+    <div 
+        v-intersection-observer="onIntersectionObserver" 
+        v-editable="blok" 
+        :class="['w-full', animationClasses]" 
+        :style="transitionDelay"
+    >
+        <div class="flex mb-sm">
+            <NuxtImg 
+                v-if="props.blok.logo.filename"
+                :src="props.blok.logo.filename" 
+                provider="storyblok" 
+                preset="icon"
+                format="webp" 
+                loading="lazy" 
+                :title="props.blok.logo.title" 
+                :alt="props.blok.logo.alt" 
+                class="my-auto mr-mdsm max-w-[theme(spacing.lg)] max-h-[theme(spacing.lg)] !object-contain" 
+            />
+            {{ props.blok.skill }}
+        </div>
+        <div            
+            class="w-full bg-gray-700 rounded-full h-sm"
+        >
+            <span 
+                :class="['block bg-primary h-full rounded-full', isInViewport ? 'transition-all duration-1000' : '']"
+                :style="[
+                    'width: ' + skillLevel + '%',
+                    transitionDelay
+                ]"
+            ></span>
+        </div>       
+    </div>
+</template>
---
+++ storyblok/Skills.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps<{
+    blok: {
+        skills: {},
+        delay_in_ms: number
+    }
+}>()
+</script>
+
+<template>
+    <section 
+        v-editable="blok"
+        class="grid gap-lg w-full md:grid-cols-2 xl:grid-cols-3 xl:gap-xl"
+    >
+        <SkillMeter
+            v-for="(skill, index) in props.blok.skills"
+            :key="skill"
+            :blok="skill"
+            :delay="index * props.blok.delay_in_ms"
+        />
+    </section>
+</template>
