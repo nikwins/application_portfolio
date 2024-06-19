39b5e0a486913e2a36098b029ab4d741ba28d6c5
Niklas Wingender <business@niklas-wingender.de>
2024-06-19 13:50:10 +0200

    [TASK] Add Content component and add styles

---
+++ storyblok/Content.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = defineProps<{
+    blok: {
+        content: {
+            text: string,
+            type: string
+        },
+        highlight_bolds: boolean,
+        size: string
+    }
+}>()
+
+const resolvedContent = computed(() => renderRichText(props.blok.content))
+</script>
+<template>
+    <div 
+        v-editable="props.blok" 
+        v-html="resolvedContent" 
+        :class="[
+            'styled-content',
+            props.blok.highlight_bolds ? '[&>p>b]:text-primary [&>p>strong]:text-primary' : '',
+            props.blok.size != 'base' && props.blok.size !== undefined ? 'text-' + props.blok.size : 'text-base'
+        ]"></div>
+</template>
