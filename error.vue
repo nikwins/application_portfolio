a437d546966c5a30cda38e766d80e6540e937520
Niklas Wingender <business@niklas-wingender.de>
2024-06-19 14:33:30 +0200

    [TASK] Add error page

---
+++ error.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+    error: Object as () => NuxtError
+})
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
+
+<template>
+    <div class="container text-center">
+        <div class="h-screen flex flex-col flex-wrap justify-center items-center">
+            <h1 class="text-primary" style="font-size: 10rem;">{{ error.statusCode }}</h1>
+            <p class="lead">{{ $t('general.errorText') }}</p>
+            <BasicButton @click="handleError" :text="$t('general.backToStart')" class="mt-xl"></BasicButton>
+        </div>
+    </div>
+</template>
