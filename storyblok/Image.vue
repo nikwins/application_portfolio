98d00e6c9c2934ff7c32d1812185312b9fde2e13
Niklas Wingender <business@niklas-wingender.de>
2024-06-19 15:51:36 +0200

    [TASK] Optimize Image template

--- storyblok/Image.vue
+++ storyblok/Image.vue
@@ -27,18 +27,8 @@ const fileExtension = computed(() => {
         :style="props.blok.image_dimensions != 'original' ? 'aspect-ratio: ' + props.blok.image_dimensions : ''"
     >
         <NuxtImg 
-            v-if="fileExtension != 'gif'"
             :src="props.blok.image.filename"
-            format="webp" 
-            provider="storyblok" 
-            sizes="320px sm:576px md:786px lg:1024px xl:1280px 2xl:1536px" 
-            loading="lazy" 
-            :title="props.blok.image.title" 
-            :alt="props.blok.image.alt" 
-            class="!h-full w-full object-cover" />
-        <NuxtImg 
-            v-if="fileExtension == 'gif'"
-            :src="props.blok.image.filename" 
+            :format="fileExtension != 'gif' ? 'webp' : false"
             provider="storyblok" 
             sizes="320px sm:576px md:786px lg:1024px xl:1280px 2xl:1536px" 
             loading="lazy" 
