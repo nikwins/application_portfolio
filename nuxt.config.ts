7e18baa6e8075f1d3c9b912154d93333bc955612
Niklas Wingender <business@niklas-wingender.de>
2024-06-20 14:51:49 +0200

    [TASK] Add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,12 +19,13 @@ export default defineNuxtConfig({
                 { name: 'format-detection', content: 'telephone=no' },
 
                 // Favicon
-                // { name: 'msapplication-TileColor', content: '#110F11' },
-                // { name: 'msapplication-config', content: '/favicon/browserconfig.xml' },
-                // { name: 'theme-color', content: '#ffffff' },
+                { name: 'msapplication-TileColor', content: '#2dd4bf' },
+                { name: 'msapplication-config', content: '/favicon/browserconfig.xml' },
+                { name: 'theme-color', content: '#ffffff' },
 
                 // SEO
                 { name: 'description', content: seo.description },
+                
                 // Open Graph / Facebook
                 { property: 'og:type', content: 'website' },
                 { property: 'og:url', content: seo.website },
@@ -38,6 +39,20 @@ export default defineNuxtConfig({
                 { property: 'twitter:title', name: 'twitter:title', content: seo.title },
                 { property: 'twitter:description', name: 'twitter:description', content: seo.description },
                 { property: 'twitter:image', name: 'twitter:image', content: seo.image },
+            ],
+            link: [
+                // Favicon
+                { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+                { rel: 'icon', sizes: '32x32', type: 'image/png', href: '/favicon/favicon-32x32.png' },
+                { rel: 'icon', sizes: '16x16', type: 'image/png', href: '/favicon/favicon-16x16.png' },
+                { rel: 'manifest', href: '/favicon/site.webmanifest' },
+                { rel: 'mask-icon', color: '#2dd4bf', href: '/favicon/safari-pinned-tab.svg' },
+                { rel: 'shortcut icon', href: '/favicon/favicon.ico' },
+
+                // Storyblok Preconnects
+                { rel: 'preconnect', href: 'https://a.storyblok.com' },
+                { rel: 'preconnect', href: 'https://a2.storyblok.com' },
+                { rel: 'preconnect', href: 'https://api.storyblok.com' },
             ],
             htmlAttrs: {
                 lang: 'de',
